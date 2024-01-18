9f6ea511fce020234f50d6b87b840f6c642c1c3e
Guilherme Turto <97317148+GuilhermeTurto@users.noreply.github.com>
2024-01-18 07:37:24 -0300

    ...

--- processar-dados.php
+++ processar-dados.php
@@ -27,3 +27,15 @@ $smtp->close();
 $conn->close();
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mensagem enviada!</title>
+</head>
+<body>
+  <h1>Mensagem enviada com sucesso!</h1>
+</body>
+</html>
